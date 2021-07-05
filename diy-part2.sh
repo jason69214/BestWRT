791af018e969b3f56a6e625eb02b91e9d0e48058
jason69214 <59967486+jason69214@users.noreply.github.com>
2021-07-04 23:06:16 -0400

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,12 +9,13 @@
 #============================================================
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/lean/default-settings/files/zzz-default-settings
 sed -i 's/OpenWrt/AutoBuild $(date "+%Y.%m.%d") @ OpenWrt/g' package/lean/default-settings/files/zzz-default-settings
-# git clone https://github.com/sypopo/luci-theme-atmaterial.git package/lean/luci-theme-atmaterial
+git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git package/lean/luci-theme-infinityfreedom
+
 #git clone https://github.com/tzxiaozhen88/luci-app-koolproxyR.git package/lean/luci-app-koolproxyR
 # git clone https://github.com/vernesong/OpenClash.git package/lean/luci-app-openclash 
 # git clone https://github.com/lisaac/luci-lib-docker.git package/luci-lib-docker
 # git clone https://github.com/lisaac/luci-app-dockerman.git package/luci-app-dockerman
-#git clone https://github.com/fw876/helloworld.git package/lean/luci-app-helloworld
+git clone https://github.com/fw876/helloworld.git package/lean/luci-app-helloworld
